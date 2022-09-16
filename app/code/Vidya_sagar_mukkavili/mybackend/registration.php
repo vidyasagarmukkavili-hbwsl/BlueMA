764548f9acb15b4b4d1765727073a6622863993f
Vidya Sagar Mukkavili <vidyasagar.mukkavili@hbwsl.com>
2022-09-16 14:14:15 +0530

    Add Custom Theme and Logo to Back-end

---
+++ app/code/Vidya_sagar_mukkavili/mybackend/registration.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Vidya_sagar_mukkavili_mybackend',
+    __DIR__
+);
---
+++ app/design/adminhtml/Vidya_sagar_mukkavili/mybackend/registration.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'adminhtml/Vidya_sagar_mukkavili/mybackend',
+    __DIR__
+);
